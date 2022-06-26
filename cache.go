54f73b1098d33e6121d3dba3e95d38eb66a2b533
Julia <juliakirillova98@gmail.com>
2022-06-26 14:20:39 +0300

    v1

--- cache.go
+++ cache.go
@@ -2,22 +2,64 @@ package cache
 
 import "time"
 
+type keyPair struct {
+	value    string
+	deadline time.Time
+}
+
 type Cache struct {
+	keyPairs map[string]keyPair
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		keyPairs: make(map[string]keyPair),
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c *Cache) Get(key string) (string, bool) {
+	cache, ok := c.keyPairs[key]
+	currentTime := time.Now()
 
+	if !cache.deadline.IsZero() && currentTime.After(cache.deadline) {
+		delete(c.keyPairs, key)
+
+		return "", false
+	}
+
+	if !ok {
+		return "", false
+	}
+
+	return cache.value, true
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.keyPairs[key] = keyPair{
+		value:    value,
+		deadline: time.Time{},
+	}
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	keys := make([]string, 0, len(c.keyPairs))
+	currentTime := time.Now()
+
+	for k, val := range c.keyPairs {
+		if !val.deadline.IsZero() && currentTime.After(val.deadline) {
+			delete(c.keyPairs, k)
+
+			continue
+		}
+		keys = append(keys, k)
+	}
+
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.keyPairs[key] = keyPair{
+		value:    value,
+		deadline: deadline,
+	}
 }
